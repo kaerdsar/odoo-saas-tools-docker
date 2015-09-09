55e1a426d1df8d4cdab6112c94c485b7a8af3c7a
Cesar Lage <kaerdsar@gmail.com>
2015-09-09 13:00:39 -0400

    fix access environment variables in python

--- makedb.py
+++ makedb.py
@@ -4,13 +4,13 @@ import erppeek
 client = erppeek.Client('http://localhost:8069')
 
 # Portal Database
-portaldb = os.environ['$MAIN_DOMAIN']
+portaldb = os.environ['MAIN_DOMAIN']
 client.create_database('admin', portaldb)
 client.login('admin', 'admin', portaldb)
 client.install('saas_portal_docker')
 
 # Server Database
-serverdb = os.environ['$SERVER_SUBDOMAIN']
+serverdb = os.environ['SERVER_SUBDOMAIN']
 client.create_database('admin', serverdb)
 client.login('admin', 'admin', serverdb)
 client.install('saas_server_docker')
