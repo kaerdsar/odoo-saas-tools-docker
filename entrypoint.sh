93393dfcc51560623162e6e13bf1bf49976ceaf3
Cesar Lage <kaerdsar@gmail.com>
2015-09-11 09:45:26 -0400

    using psycopg2 to create database from odoo container

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,12 @@ set -e
 # Set odoo database host, port, user and password
 : ${PGHOST:=${RDS_HOSTNAME:=${PGHOST:='localhost'}}}
 : ${PGPORT:=${RDS_PORT:=${PGPORT:=5432}}}
-: ${PGUSER:=${RDS_USERNAME:=${PGUSER:='postgres'}}}
+: ${PGDB:=${RDS_DB_NAME:=${PGDB:='postgres'}}}
+: ${PGUSER:=${RDS_USERNAME:=${PGUSER:='odoo'}}}
 : ${PGPASSWORD:=${RDS_PASSWORD:=${PGPASSWORD}}}
-export PGHOST PGPORT PGUSER PGPASSWORD
+export PGHOST PGPORT PGUSER PGPASSWORD PGDB
+
+python /etc/odoo/makedb.py
 
 # Generate UUID for the server database
 uuid=$(cat /proc/sys/kernel/random/uuid)
@@ -16,6 +19,10 @@ sed -i "s/odoo.local/$SERVER_SUBDOMAIN:8069/g" /mnt/odoo-saas-tools/saas_server/
 sed -i "s/server_subdomain/$SERVER_SUBDOMAIN/g" /mnt/odoo-saas-tools/saas_portal_docker/data/server.xml
 sed -i "s/server_client_id/$uuid/g" /mnt/odoo-saas-tools/saas_portal_docker/data/server.xml
 sed -i "s/server_client_id/$uuid/g" /mnt/odoo-saas-tools/saas_server_docker/data/provider.xml
+
+# Install docker modules on portal and server database
+openerp-server -c /etc/odoo/openerp-server.conf -d $SERVER_SUBDOMAIN -i saas_server_docker --without-demo=all --stop-after-init
+openerp-server -c /etc/odoo/openerp-server.conf -d $MAIN_DOMAIN -i saas_portal_docker --without-demo=all --stop-after-init
 
 case "$1" in
 	--)
@@ -28,7 +35,5 @@ case "$1" in
 	*)
 		exec "$@"
 esac
-
-python /etc/odoo/makedb.py
 
 exit 1
