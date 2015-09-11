7cef875ae29e518e5ab1f9ebb730c8abc4b6c9e2
Cesar Lage <kaerdsar@gmail.com>
2015-09-11 13:42:14 -0400

    check if main_database already exists

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,17 +12,20 @@ export PGHOST PGPORT PGUSER PGPASSWORD PGDB
 
 python /etc/odoo/makedb.py
 
-# Generate UUID for the server database
-uuid=$(cat /proc/sys/kernel/random/uuid)
-# Update docker addons for odoo
-sed -i "s/odoo.local/$SERVER_SUBDOMAIN:8069/g" /mnt/odoo-saas-tools/saas_server/data/ir_config_parameter.xml
-sed -i "s/server_subdomain/$SERVER_SUBDOMAIN/g" /mnt/odoo-saas-docker/saas_portal_docker/data/server.xml
-sed -i "s/server_client_id/$uuid/g" /mnt/odoo-saas-docker/saas_portal_docker/data/server.xml
-sed -i "s/server_client_id/$uuid/g" /mnt/odoo-saas-docker/saas_server_docker/data/provider.xml
+if [ $? -eq 1 ]
+then
+	# Generate UUID for the server database
+	uuid=$(cat /proc/sys/kernel/random/uuid)
+	# Update docker addons for odoo
+	sed -i "s/odoo.local/$SERVER_SUBDOMAIN:8069/g" /mnt/odoo-saas-tools/saas_server/data/ir_config_parameter.xml
+	sed -i "s/server_subdomain/$SERVER_SUBDOMAIN/g" /mnt/odoo-saas-docker/saas_portal_docker/data/server.xml
+	sed -i "s/server_client_id/$uuid/g" /mnt/odoo-saas-docker/saas_portal_docker/data/server.xml
+	sed -i "s/server_client_id/$uuid/g" /mnt/odoo-saas-docker/saas_server_docker/data/provider.xml
 
-# Install docker modules on portal and server database
-openerp-server -c /etc/odoo/openerp-server.conf -d $SERVER_SUBDOMAIN -i saas_server_docker --without-demo=all --stop-after-init
-openerp-server -c /etc/odoo/openerp-server.conf -d $MAIN_DOMAIN -i saas_portal_docker --without-demo=all --stop-after-init
+	# Install docker modules on portal and server database
+	openerp-server -c /etc/odoo/openerp-server.conf -d $SERVER_SUBDOMAIN -i saas_server_docker --without-demo=all --stop-after-init
+	openerp-server -c /etc/odoo/openerp-server.conf -d $MAIN_DOMAIN -i saas_portal_docker --without-demo=all --stop-after-init
+fi
 
 case "$1" in
 	--)
--- makedb.py
+++ makedb.py
@@ -1,19 +1,33 @@
 import os
+import sys
 
 from psycopg2 import connect
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
-conn = connect(
-    dbname=os.environ['PGDB'],
-    user=os.environ['PGUSER'],
-    host=os.environ['PGHOST'],
-    port=os.environ['PGPORT'],
-    password=os.environ['PGPASSWORD'],
-)
+st = 0
+try:
+    conn = connect(
+        dbname=os.environ['MAIN_DOMAIN'],
+        user=os.environ['PGUSER'],
+        host=os.environ['PGHOST'],
+        port=os.environ['PGPORT'],
+        password=os.environ['PGPASSWORD'],
+    )
+except:
+    conn = connect(
+        dbname=os.environ['PGDB'],
+        user=os.environ['PGUSER'],
+        host=os.environ['PGHOST'],
+        port=os.environ['PGPORT'],
+        password=os.environ['PGPASSWORD'],
+    )
 
-conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-cr = conn.cursor()
-cr.execute('CREATE DATABASE "{}" OWNER {};'.format(os.environ['MAIN_DOMAIN'], os.environ['PGUSER']))
-cr.execute('CREATE DATABASE "{}" OWNER {};'.format(os.environ['SERVER_SUBDOMAIN'], os.environ['PGUSER']))
-cr.close()
-conn.close()
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cr = conn.cursor()
+    cr.execute('CREATE DATABASE "{}" OWNER {};'.format(os.environ['MAIN_DOMAIN'], os.environ['PGUSER']))
+    cr.execute('CREATE DATABASE "{}" OWNER {};'.format(os.environ['SERVER_SUBDOMAIN'], os.environ['PGUSER']))
+    cr.close()
+    conn.close()
+    st = 1
+
+sys.exit(st)
